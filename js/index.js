29e3a91fb0233da81f4c279f1652ce6f7a229008
Hao C <impyworm@gmail.com>
2022-09-22 22:11:32 -0600

    Add about section text

--- js/index.js
+++ js/index.js
@@ -5,3 +5,30 @@ year.innerText = (() => {
 
     return currentYear;
 })();
+
+const aboutSection = document.querySelector("#about");
+const wave = document.querySelector("#about > img.wave");
+
+function setAboutPadding() {
+    aboutSection.style.paddingTop = `${(wave.offsetHeight) * (35 / 100)}px`;
+
+    console.log(aboutSection.style.paddingTop, wave.offsetHeight);
+}
+
+window.addEventListener("resize", () => {
+    setAboutPadding();
+});
+
+setAboutPadding();
+
+const creditsBtn = document.querySelector(".links .btn:nth-child(2)");
+const modal = document.querySelector(".modal")
+const modalClose = document.querySelector("span.close");
+
+creditsBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+});
+
+modalClose.addEventListener("click", () => {
+    modal.classList.toggle("show");
+});
