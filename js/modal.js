b592a1b593e47870efe393aa5cd9d2c2dcdc0751
Hao C <impyworm@gmail.com>
2022-10-10 21:28:17 -0600

    Complete website

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 const creditsBtn = document.querySelector(".links .btn:nth-child(2)");
-const modal = document.querySelector(".modal");
+const modal = document.querySelector(".modal.credits");
 const modalClose = document.querySelector("span.close");
 
 creditsBtn.addEventListener("click", () => {
